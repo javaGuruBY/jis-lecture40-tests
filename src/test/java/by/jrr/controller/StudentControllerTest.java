3214af50d724192695dcb6ee4786e334b5c0d14d
max-java <6666350@gmail.com>
2021-01-05 23:16:08 +0300

    adds StudentController integration test

--- src/test/java/by/jrr/controller/StudentControllerTest.java
+++ src/test/java/by/jrr/controller/StudentControllerTest.java
@@ -1,24 +1,38 @@
 package by.jrr.controller;
 
+import by.jrr.config.DataBaseConfig;
 import by.jrr.config.SpringWebAppInitializer;
 import by.jrr.config.WebConfig;
+import by.jrr.repository.StudentRepository;
+import by.jrr.service.StudentService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.client.match.MockRestRequestMatchers;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.ModelResultMatchers;
+import org.springframework.test.web.servlet.result.StatusResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {SpringWebAppInitializer.class, WebConfig.class})
+@ContextConfiguration(classes = {SpringWebAppInitializer.class,
+        WebConfig.class,
+        StudentRepository.class,
+        StudentService.class,
+        StudentController.class,
+        DataBaseConfig.class})
 @WebAppConfiguration
 public class StudentControllerTest {
 
@@ -35,6 +49,8 @@ public class StudentControllerTest {
     @Test
     public void openIndex() throws Exception {
 //        RequestBuilder requestBuilder
-        mockMvc.perform(get("/")).andDo(print());
+        mockMvc.perform(get("/"))
+                .andDo(print())
+        .andExpect(status().is2xxSuccessful());
     }
 }
